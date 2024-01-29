4cf203f1af1331c2176fa7d1f095a6d9787f59dd
Austin Hunter <ahunter8850@outlook.com>
2024-01-29 16:08:14 -0600

    finicity

--- src/app/services/plaid.service.ts
+++ src/app/services/plaid.service.ts
@@ -11,7 +11,11 @@ import {
   REMOVE_LINKED_ACCOUNT_URL,
   CREATE_PLAID_UPDATE_LINK_TOKEN_URL,
 } from '../constants/http/urls';
-import { PlaidHandler, PlaidOnSuccessMetadata, PlaidTransaction } from '../types/plaid/plaid';
+import {
+  PlaidHandler,
+  PlaidOnSuccessMetadata,
+  PlaidTransaction,
+} from '../types/plaid/plaid';
 import { plaidTransactionToFirestoreTransaction } from '../helpers/mappers/transactions';
 import { TransactionPublisherService } from './transaction-publisher.service';
 import { LoadingController } from '@ionic/angular';
@@ -21,7 +25,7 @@ import { UserService } from './user.service';
 declare var Plaid: any;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlaidService {
   public get plaidAvailable(): boolean {
@@ -34,29 +38,30 @@ export class PlaidService {
     private userRepository: UserRepositoryService,
     private transactionPublisher: TransactionPublisherService,
     private loadingController: LoadingController,
-    private userService: UserService,
-  ) {
-  }
+    private userService: UserService
+  ) {}
 
   async linkPlaidToUser(): Promise<void> {
     if (!this.plaidAvailable) return;
     const loader = await this.loadingController.create();
     await loader.present();
-    this.http.post(
-      CREATE_PLAID_LINK_TOKEN_URL,
-      {
+    this.http
+      .post(CREATE_PLAID_LINK_TOKEN_URL, {
         user_id: this.userRepository.getCurrentUserId()!,
-      }
-    ).pipe(catchError((err) => {
-      console.log(err);
-      loader.dismiss();
-      return err;
-    })).subscribe((resp) => {
-      console.log(resp);
-      const handler = this.createPlaidHandlerLink(resp.link_token);
-      loader.dismiss();
-      handler.open();
-    });
+      })
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          loader.dismiss();
+          return err;
+        })
+      )
+      .subscribe((resp) => {
+        console.log(resp);
+        const handler = this.createPlaidHandlerLink(resp.link_token);
+        loader.dismiss();
+        handler.open();
+      });
   }
 
   async syncPlaidTransactions(): Promise<void> {
@@ -65,29 +70,26 @@ export class PlaidService {
     if (!userId) throw new Error('User is not logged in');
 
     /* Step 1: Get the user's linked accounts */
-    const linkedAccounts = (await this.linkedAccountsRepository.getAllFromParent(
-      userId,
-    )).docs;
-    if (linkedAccounts.length === 0)
-      return;
+    const linkedAccounts = (
+      await this.linkedAccountsRepository.getAllFromParent(userId)
+    ).docs;
+    if (linkedAccounts.length === 0) return;
 
     /* Step 2: Get the transactions from Plaid */
     const promises: Promise<any>[] = [];
     for (let linkedAccount of linkedAccounts) {
-      if (!(await this.shouldSyncTransactions(linkedAccount)))
-        continue;
-      promises.push(
-        lastValueFrom(
-          this.http.post(
-            GET_TRANSACTION_DATA_URL,
-            {
+      if (linkedAccount.isPlaid) {
+        if (!(await this.shouldSyncTransactions(linkedAccount))) continue;
+        promises.push(
+          lastValueFrom(
+            this.http.post(GET_TRANSACTION_DATA_URL, {
               userId,
               linkedAccount: linkedAccount,
               linkedAccountId: linkedAccount.id!,
-            }
+            })
           )
-        )
-      );
+        );
+      }
     }
     const responses = await Promise.all(promises);
 
@@ -97,21 +99,15 @@ export class PlaidService {
     let modifiedTransactions: PlaidTransaction[] = [];
     for (let i = 0; i < responses.length; i++) {
       const resp = responses[i];
-      addedTransactions = addedTransactions.concat(
-        resp.added
-      );
-      removedTransactions = removedTransactions.concat(
-        resp.removed
-      );
-      modifiedTransactions = modifiedTransactions.concat(
-        resp.modified
-      );
+      addedTransactions = addedTransactions.concat(resp.added);
+      removedTransactions = removedTransactions.concat(resp.removed);
+      modifiedTransactions = modifiedTransactions.concat(resp.modified);
     }
     this.plaidTransactionSyncHandler(
       addedTransactions,
       removedTransactions,
       modifiedTransactions,
-      true,     // publishTransactionEvent
+      true // publishTransactionEvent
     );
   }
 
@@ -119,43 +115,46 @@ export class PlaidService {
     const userId = this.userRepository.getCurrentUserId();
     if (!userId) throw new Error('User is not logged in');
 
-    this.http.post(
-      REMOVE_LINKED_ACCOUNT_URL,
-      {
+    this.http
+      .post(REMOVE_LINKED_ACCOUNT_URL, {
         user_id: userId,
-        linked_account_id: linkedAccount.id
-      }
-    ).subscribe((resp) => {
-      console.log(resp);
-    });
+        linked_account_id: linkedAccount.id,
+      })
+      .subscribe((resp) => {
+        console.log(resp);
+      });
   }
 
-  async relinkPlaidLinkedAccount(linkedAccount: LinkedAccount, successCallback?: (linkedAccountId: string) => void):
-    Promise<void> {
+  async relinkPlaidLinkedAccount(
+    linkedAccount: LinkedAccount,
+    successCallback?: (linkedAccountId: string) => void
+  ): Promise<void> {
     if (!this.plaidAvailable) return;
     const loader = await this.loadingController.create();
     await loader.present();
-    this.http.post(
-      CREATE_PLAID_UPDATE_LINK_TOKEN_URL,
-      {
+    this.http
+      .post(CREATE_PLAID_UPDATE_LINK_TOKEN_URL, {
         user_id: this.userRepository.getCurrentUserId()!,
         linked_account_id: linkedAccount.id!,
-      }
-    ).pipe(catchError((err) => {
-      console.log(err);
-      loader.dismiss();
-      return err;
-    })).subscribe((resp) => {
-      console.log(resp);
-      const handler = this.createPlaidHandlerRelink(
-        resp.link_token,
-        this.userRepository.getCurrentUserId()!,
-        linkedAccount.id!,
-        successCallback,
-      );
-      loader.dismiss();
-      handler.open();
-    });
+      })
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          loader.dismiss();
+          return err;
+        })
+      )
+      .subscribe((resp) => {
+        console.log(resp);
+        const handler = this.createPlaidHandlerRelink(
+          resp.link_token,
+          this.userRepository.getCurrentUserId()!,
+          linkedAccount.id!,
+          successCallback
+        );
+        loader.dismiss();
+        handler.open();
+      });
   }
 
   private createPlaidHandlerLink(token: string): PlaidHandler {
@@ -173,26 +172,22 @@ export class PlaidService {
     token: string,
     userId: string,
     linkedAccountId?: string,
-    successCallback?: (linkedAccountId: string) => void,
+    successCallback?: (linkedAccountId: string) => void
   ): PlaidHandler {
     return Plaid.create({
       token,
       // On success, change the required action to NONE
       onSuccess: (publicToken: string, metadata: PlaidOnSuccessMetadata) => {
-        if (!linkedAccountId)
-          return;
-        this.linkedAccountsRepository.update(
-          userId,
-          linkedAccountId,
-          {
+        if (!linkedAccountId) return;
+        this.linkedAccountsRepository
+          .update(userId, linkedAccountId, {
             link_status: {
               required_action: 'NONE',
             },
-          }
-        ).then((success: boolean) => {
-          if (success && successCallback)
-            successCallback(linkedAccountId);
-        });
+          })
+          .then((success: boolean) => {
+            if (success && successCallback) successCallback(linkedAccountId);
+          });
       },
       onExit: this.plaidHandlerOnExitCallback.bind(this),
       onLoad: this.plaidHandlerOnLoadCallback.bind(this),
@@ -201,58 +196,65 @@ export class PlaidService {
     });
   }
 
-  private plaidHandlerOnSuccessCallback(publicToken: string, metadata: PlaidOnSuccessMetadata): void {
-    this.http.post(
-      EXCHANGE_PLAID_PUBLIC_TOKEN_URL,
-      {
-        publicToken,
-        institutionName: metadata.institution.name,
-        userId: this.userRepository.getCurrentUserId()!,
-      },
-      /* TODO: Change EXCHANGE_PLAID_PUBLIC_TOKEN_URL to return
-      * the transactions of current day. Make backend store these
-      * fields because they are secret. */
-    ).subscribe((resp) => {
-      console.log(resp);
-      const {
-        added,
-        removed,
-        modified,
-      } = resp;
-      this.plaidTransactionSyncHandler(
-        added,
-        removed,
-        modified,
-        true,      // publishTransactionEvent
-      );
-    });
+  private plaidHandlerOnSuccessCallback(
+    publicToken: string,
+    metadata: PlaidOnSuccessMetadata
+  ): void {
+    this.http
+      .post(
+        EXCHANGE_PLAID_PUBLIC_TOKEN_URL,
+        {
+          publicToken,
+          institutionName: metadata.institution.name,
+          userId: this.userRepository.getCurrentUserId()!,
+        }
+        /* TODO: Change EXCHANGE_PLAID_PUBLIC_TOKEN_URL to return
+         * the transactions of current day. Make backend store these
+         * fields because they are secret. */
+      )
+      .subscribe((resp) => {
+        console.log(resp);
+        const { added, removed, modified } = resp;
+        this.plaidTransactionSyncHandler(
+          added,
+          removed,
+          modified,
+          true // publishTransactionEvent
+        );
+      });
   }
 
-  private async plaidHandlerOnExitCallback(error: object | null, metadata: object): Promise<void> {
+  private async plaidHandlerOnExitCallback(
+    error: object | null,
+    metadata: object
+  ): Promise<void> {
     console.log(error);
   }
 
-  private async plaidHandlerOnLoadCallback(): Promise<void> {
-  }
+  private async plaidHandlerOnLoadCallback(): Promise<void> {}
 
-  private async plaidHandlerOnEventCallback(eventName: string, metadata: object): Promise<void> {
-  }
+  private async plaidHandlerOnEventCallback(
+    eventName: string,
+    metadata: object
+  ): Promise<void> {}
 
   /** Function that takes transactions from sync data and handles them according
-    * to the parameters passed in.
-    */
+   * to the parameters passed in.
+   */
   private async plaidTransactionSyncHandler(
     addedTransactions: PlaidTransaction[],
     removedTransactions: PlaidTransaction[],
     modifiedTransactions: PlaidTransaction[],
-    publishTransactionEvent: boolean = true,
+    publishTransactionEvent: boolean = true
   ): Promise<TransactionEvent> {
-    const addedTransactionsFirestore: Transaction[] = addedTransactions.
-      map(plaidTransactionToFirestoreTransaction);
-    const removedTransactionsFirestore: Transaction[] = removedTransactions.
-      map(plaidTransactionToFirestoreTransaction);
-    const modifiedTransactionsFirestore: Transaction[] = modifiedTransactions.
-      map(plaidTransactionToFirestoreTransaction);
+    const addedTransactionsFirestore: Transaction[] = addedTransactions.map(
+      plaidTransactionToFirestoreTransaction
+    );
+    const removedTransactionsFirestore: Transaction[] = removedTransactions.map(
+      plaidTransactionToFirestoreTransaction
+    );
+    const modifiedTransactionsFirestore: Transaction[] =
+      modifiedTransactions.map(plaidTransactionToFirestoreTransaction);
 
     const event: TransactionEvent = {
       from: 'plaid',
@@ -266,7 +268,9 @@ export class PlaidService {
     return event;
   }
 
-  private async shouldSyncTransactions(linkedAccount: LinkedAccount): Promise<boolean> {
+  private async shouldSyncTransactions(
+    linkedAccount: LinkedAccount
+  ): Promise<boolean> {
     if (!linkedAccount.last_transaction_retrieval) return true;
     const now = new Date();
     const lastSync = linkedAccount.last_transaction_retrieval;
